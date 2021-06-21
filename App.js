1547b3d34d4dbb5cc3dc63beb679a1d76c6de4f3
lucas.choi <lucas.choi@ibetalabs.com>
2021-06-21 18:08:14 +0900

    Style02 까지 완료

--- App.js
+++ App.js
@@ -35,14 +35,14 @@ const App: () => Node = () => {
 
   return (
     <View style={styles.mainView}>
-        <View>
+        <View style={styles.subView}>
+            <Text style={styles.mainText}>Hello World</Text>
+        </View>
+        <View style={styles.subView}>
             <Text>Hello World</Text>
         </View>
-        <View>
-            <Text>Hello World</Text>
-        </View>
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.anotherSubView}>
+            <Text style={styles.mainText}>Hello World</Text>
         </View>
     </View>
   );
@@ -55,7 +55,27 @@ const styles = StyleSheet.create({
       height: "100%",
       alignItems: "center",
       justifyContent: "center"
-  }
+  },
+  subView: {
+      flex: 1,
+      backgroundColor: "yellow",
+      marginBottom: 10,
+      width: "50%"
+  },
+  anotherSubView: {
+      flex: 2,
+      backgroundColor: "yellow",
+      marginBottom: 10,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center"
+  },
+    mainText: {
+      fontSize: 50,
+      fontWeight: "bold",
+        color: "red",
+        padding: 20
+    }
 });
 
 export default App;
